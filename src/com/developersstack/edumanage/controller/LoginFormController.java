ff6d3c03f81e9e48b133fbf1256d4e1aec6184ad
ozen-hub <info.ozenhub@gmail.com>
2023-03-20 21:10:06 +0530

    3

--- src/com/developersstack/edumanage/controller/LoginFormController.java
+++ src/com/developersstack/edumanage/controller/LoginFormController.java
@@ -1,4 +1,21 @@
 package com.developersstack.edumanage.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
 public class LoginFormController {
+    public AnchorPane context;
+    public TextField txtEmail;
+    public PasswordField txtPassword;
+
+    public void forgotPasswordOnAction(ActionEvent actionEvent) {
+    }
+
+    public void loginOnAction(ActionEvent actionEvent) {
+    }
+
+    public void createAnAccountOnAction(ActionEvent actionEvent) {
+    }
 }
