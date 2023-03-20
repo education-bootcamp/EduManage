71ac0d79dfc7aa668e1752015f8d70ac7aa09422
ozen-hub <info.ozenhub@gmail.com>
2023-03-20 22:21:19 +0530

    8

--- src/com/developersstack/edumanage/controller/LoginFormController.java
+++ src/com/developersstack/edumanage/controller/LoginFormController.java
@@ -1,8 +1,11 @@
 package com.developersstack.edumanage.controller;
 
+import com.developersstack.edumanage.db.Database;
+import com.developersstack.edumanage.model.User;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
@@ -10,6 +13,7 @@ import javafx.stage.Stage;
 import javafx.stage.Window;
 
 import java.io.IOException;
+import java.util.Optional;
 
 public class LoginFormController {
     public AnchorPane context;
@@ -20,6 +24,36 @@ public class LoginFormController {
     }
 
     public void loginOnAction(ActionEvent actionEvent) {
+        String email = txtEmail.getText().toLowerCase();
+        String password = txtPassword.getText().trim();
+
+       /* for (User user : Database.userTable){
+            if (user.getEmail().equals(email)){
+               if (user.getPassword().equals(password)){
+                   System.out.println(user.toString());
+                   return;
+               }else{
+                   new Alert(Alert.AlertType.ERROR,
+                          "Wrong Password!").show();
+                   return;
+               }
+            }
+        }
+        new Alert(Alert.AlertType.WARNING,
+                String.format("user not found (%s)",email)).show();*/
+       Optional<User> selectedUser =
+               Database.userTable.stream().filter(e->e.getEmail().equals(email)).findFirst();
+       if (selectedUser.isPresent()){
+           if (selectedUser.get().getPassword().equals(password)){
+               System.out.println(selectedUser.get().toString());
+           }else{
+               new Alert(Alert.AlertType.ERROR,
+                       "Wrong Password!").show();
+           }
+       }else{
+           new Alert(Alert.AlertType.WARNING,
+                   String.format("user not found (%s)",email)).show();
+       }
     }
 
     public void createAnAccountOnAction(ActionEvent actionEvent) throws IOException {
--- src/com/developersstack/edumanage/model/User.java
+++ src/com/developersstack/edumanage/model/User.java
@@ -47,4 +47,18 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+    /*@Override
+    public String toString() {
+        return this.getClass().getName()+"@"+Integer.toHexString(hashCode());
+    }*/
 }
