6fd8efee0bc52c83f3c5e86ccd91dd0b9a82736c
ozen-hub <info.ozenhub@gmail.com>
2023-03-20 21:16:51 +0530

    4

--- src/com/developersstack/edumanage/controller/LoginFormController.java
+++ src/com/developersstack/edumanage/controller/LoginFormController.java
@@ -1,9 +1,15 @@
 package com.developersstack.edumanage.controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+
+import java.io.IOException;
 
 public class LoginFormController {
     public AnchorPane context;
@@ -16,6 +22,14 @@ public class LoginFormController {
     public void loginOnAction(ActionEvent actionEvent) {
     }
 
-    public void createAnAccountOnAction(ActionEvent actionEvent) {
+    public void createAnAccountOnAction(ActionEvent actionEvent) throws IOException {
+        setUi("SignupForm");
+    }
+
+    private void setUi(String location) throws IOException {
+        Stage stage = (Stage) context.getScene().getWindow();
+        stage.setScene(new Scene(
+                FXMLLoader.load(getClass().getResource("../view/"+location+".fxml"))));
+        stage.centerOnScreen();
     }
 }
