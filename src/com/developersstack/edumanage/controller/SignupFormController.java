3afc5ac7a88372a8d09e98ad0cc7eeacdb881508
ozen-hub <info.ozenhub@gmail.com>
2023-03-20 21:38:42 +0530

    6

--- src/com/developersstack/edumanage/controller/SignupFormController.java
+++ src/com/developersstack/edumanage/controller/SignupFormController.java
@@ -1,4 +1,20 @@
 package com.developersstack.edumanage.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
 public class SignupFormController {
+    public AnchorPane context;
+    public TextField txtFirstName;
+    public PasswordField txtPassword;
+    public TextField txtEmail;
+    public TextField txtLastName;
+
+    public void signUpOnAction(ActionEvent actionEvent) {
+    }
+
+    public void alreadyHaveAnAccountOnAction(ActionEvent actionEvent) {
+    }
 }
--- src/com/developersstack/edumanage/db/Database.java
+++ src/com/developersstack/edumanage/db/Database.java
@@ -1,4 +1,9 @@
 package com.developersstack.edumanage.db;
 
+import com.developersstack.edumanage.model.User;
+
+import java.util.ArrayList;
+
 public class Database {
+    public static ArrayList<User> userTable= new ArrayList();
 }
