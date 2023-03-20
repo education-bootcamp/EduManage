9cd97e7997302eca71d0ccaa8cba224c67b28748
ozen-hub <info.ozenhub@gmail.com>
2023-03-20 21:45:27 +0530

    7

--- src/com/developersstack/edumanage/controller/SignupFormController.java
+++ src/com/developersstack/edumanage/controller/SignupFormController.java
@@ -1,9 +1,17 @@
 package com.developersstack.edumanage.controller;
 
+import com.developersstack.edumanage.db.Database;
+import com.developersstack.edumanage.model.User;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class SignupFormController {
     public AnchorPane context;
@@ -12,9 +20,25 @@ public class SignupFormController {
     public TextField txtEmail;
     public TextField txtLastName;
 
-    public void signUpOnAction(ActionEvent actionEvent) {
+    public void signUpOnAction(ActionEvent actionEvent) throws IOException {
+        String email = txtEmail.getText().toLowerCase();
+        String firstName = txtFirstName.getText();
+        String lastName = txtLastName.getText();
+        String password = txtPassword.getText().trim();
+        Database.userTable.add(
+                new User(firstName,lastName,email,password)
+        );
+        new Alert(Alert.AlertType.INFORMATION, "Welcome!").show();
+        setUi("LoginForm");
     }
 
-    public void alreadyHaveAnAccountOnAction(ActionEvent actionEvent) {
+    public void alreadyHaveAnAccountOnAction(ActionEvent actionEvent) throws IOException {
+        setUi("LoginForm");
+    }
+    private void setUi(String location) throws IOException {
+        Stage stage = (Stage) context.getScene().getWindow();
+        stage.setScene(new Scene(
+                FXMLLoader.load(getClass().getResource("../view/"+location+".fxml"))));
+        stage.centerOnScreen();
     }
 }
