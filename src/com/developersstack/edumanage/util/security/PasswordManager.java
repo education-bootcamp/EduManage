71cd14040d59fc30dcf67ed3b6b95a4e55a1a9d5
ozen-hub <info.ozenhub@gmail.com>
2023-03-20 23:03:42 +0530

    9

---
+++ src/com/developersstack/edumanage/util/security/PasswordManager.java
@@ -0,0 +1,13 @@
+package com.developersstack.edumanage.util.security;
+
+
+import org.mindrot.BCrypt;
+
+public class PasswordManager {
+    public String encode(String rawPassword){
+        return BCrypt.hashpw(rawPassword,BCrypt.gensalt(10));
+    }
+    public boolean checkPassword(String rawPassword, String hashPassword){
+        return BCrypt.checkpw(rawPassword, hashPassword);
+    }
+}
