f14f1f62d2d6997c789a2ac7c0fd9579a0718294
ozen-hub <info.ozenhub@gmail.com>
2023-03-21 21:05:04 +0530

    16

--- src/com/developersstack/edumanage/controller/StudentFormController.java
+++ src/com/developersstack/edumanage/controller/StudentFormController.java
@@ -1,9 +1,15 @@
 package com.developersstack.edumanage.controller;
 
+import com.developersstack.edumanage.db.Database;
+import com.developersstack.edumanage.model.Student;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+
+import java.time.ZoneId;
+import java.util.Date;
 
 public class StudentFormController {
     public AnchorPane context;
@@ -12,6 +18,36 @@ public class StudentFormController {
     public DatePicker txtDob;
     public TextField txtAddress;
 
+    public void initialize(){
+        setStudentId();
+    }
+
+    private void setStudentId() {
+        if (!Database.studentTable.isEmpty()){
+            Student lastStudent = Database.studentTable.get(
+                    Database.studentTable.size()-1
+            );
+            String lastId= lastStudent.getStudentId();
+            String splitData[] = lastId.split("-");
+            String lastIdIntegerNumberAsAString = splitData[1];
+            int lastIntegerIdAsInt=Integer.parseInt(lastIdIntegerNumberAsAString);
+            lastIntegerIdAsInt++;
+            String generatedStudentId="S-"+lastIntegerIdAsInt;
+            txtId.setText(generatedStudentId);
+        }else{
+            txtId.setText("S-1");
+        }
+    }
+
     public void saveOnAction(ActionEvent actionEvent) {
+        Student student = new Student(
+                txtId.getText(),
+                txtName.getText(),
+                Date.from(txtDob.getValue().atStartOfDay(ZoneId.systemDefault()).toInstant()),
+                txtAddress.getText()
+        );
+        Database.studentTable.add(student);
+        setStudentId();
+        new Alert(Alert.AlertType.INFORMATION, "Student saved!").show();
     }
 }
