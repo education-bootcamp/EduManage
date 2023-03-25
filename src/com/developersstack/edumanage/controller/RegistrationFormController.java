46c633f1b7f6b0ee708888554a61fd3a39d4b661
ozen-hub <info.ozenhub@gmail.com>
2023-03-25 23:27:08 +0530

    46

--- src/com/developersstack/edumanage/controller/RegistrationFormController.java
+++ src/com/developersstack/edumanage/controller/RegistrationFormController.java
@@ -1,5 +1,37 @@
 package com.developersstack.edumanage.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class RegistrationFormController {
 
+    public AnchorPane context;
+    public TextField txtId;
+    public TextField txtStudent;
+    public Button btn;
+    public ComboBox cmbProgram;
+    public RadioButton rBtnPaid;
+
+    public void backToHomeOnAction(ActionEvent actionEvent) throws IOException {
+        setUi("DashboardForm");
+    }
+
+    public void saveOnAction(ActionEvent actionEvent) {
+    }
+
+    private void setUi(String location) throws IOException {
+        Stage stage = (Stage) context.getScene().getWindow();
+        stage.setScene(new Scene(
+                FXMLLoader.load(getClass().getResource("../view/"+location+".fxml"))));
+        stage.centerOnScreen();
+    }
 }
