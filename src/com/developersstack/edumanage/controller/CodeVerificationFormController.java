03948d510b103d0e138dde0d899393bec24a1017
ozen-hub <info.ozenhub@gmail.com>
2023-03-25 21:21:29 +0530

    37

--- src/com/developersstack/edumanage/controller/CodeVerificationFormController.java
+++ src/com/developersstack/edumanage/controller/CodeVerificationFormController.java
@@ -1,18 +1,36 @@
 package com.developersstack.edumanage.controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class CodeVerificationFormController {
     public AnchorPane context;
     public TextField txtCode;
     public Label txtSelectedEmail;
 
+    public void setUserData(int verificationCode, String email){
+        System.out.println(verificationCode);
+        System.out.println(email);
+    }
+
     public void verifyCodeOnAction(ActionEvent actionEvent) {
     }
 
-    public void changeEmailOnAction(ActionEvent actionEvent) {
+    public void changeEmailOnAction(ActionEvent actionEvent) throws IOException {
+        setUi("ForgotPasswordForm");
+    }
+
+    private void setUi(String location) throws IOException {
+        Stage stage = (Stage) context.getScene().getWindow();
+        stage.setScene(new Scene(
+                FXMLLoader.load(getClass().getResource("../view/" + location + ".fxml"))));
+        stage.centerOnScreen();
     }
 }
--- src/com/developersstack/edumanage/controller/ForgotPasswordFormController.java
+++ src/com/developersstack/edumanage/controller/ForgotPasswordFormController.java
@@ -3,10 +3,12 @@ package com.developersstack.edumanage.controller;
 import com.developersstack.edumanage.util.tools.VerificationCodeGenerator;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import javafx.stage.Window;
 
 import javax.mail.Message;
 import javax.mail.MessagingException;
@@ -50,8 +52,18 @@ public class ForgotPasswordFormController {
             System.out.println("Completed!");
             //=======================>
 
+            FXMLLoader fxmlLoader =
+                    new FXMLLoader(getClass().getResource("../view/CodeVerificationForm.fxml"));
+            Parent parent = fxmlLoader.load();
+            CodeVerificationFormController controller = fxmlLoader.getController();
+            controller.setUserData(verificationCode,txtEmail.getText());
+            Stage stage =(Stage) context.getScene().getWindow();
+            stage.setScene(new Scene(parent));
+
         } catch (MessagingException e) {
             e.printStackTrace();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
 
     }
