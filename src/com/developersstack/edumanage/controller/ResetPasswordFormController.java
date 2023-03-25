d7868e5ab596b3a4a637428e1eee8ba4a2fe23ab
ozen-hub <info.ozenhub@gmail.com>
2023-03-25 21:41:37 +0530

    40

---
+++ src/com/developersstack/edumanage/controller/ResetPasswordFormController.java
@@ -0,0 +1,43 @@
+package com.developersstack.edumanage.controller;
+
+import com.developersstack.edumanage.db.Database;
+import com.developersstack.edumanage.model.User;
+import com.developersstack.edumanage.util.security.PasswordManager;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Optional;
+
+public class ResetPasswordFormController {
+    public AnchorPane context;
+    public TextField txtPassword;
+
+    String selectedEmail = "";
+
+    public void setUserData(String email) {
+        selectedEmail = email;
+    }
+
+    public void changePasswordOnAction(ActionEvent actionEvent) throws IOException {
+        Optional<User> selectedUser = Database.userTable.stream().filter(e -> e.getEmail().equals(selectedEmail)).findFirst();
+        if (selectedUser.isPresent()){
+            selectedUser.get().setPassword(new PasswordManager().encrypt(txtPassword.getText()));
+            setUi("LoginForm");
+        }else{
+            new Alert(Alert.AlertType.ERROR, "Not Found").show();
+        }
+    }
+
+    private void setUi(String location) throws IOException {
+        Stage stage = (Stage) context.getScene().getWindow();
+        stage.setScene(new Scene(
+                FXMLLoader.load(getClass().getResource("../view/" + location + ".fxml"))));
+        stage.centerOnScreen();
+    }
+}
